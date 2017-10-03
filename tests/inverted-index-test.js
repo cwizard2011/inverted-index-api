24facd7f39e907fbc24544222771271294ba69b9
Cwizard <cwizard2011@gmail.com>
2017-10-03 11:58:25 +0100

    commit

--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -1,7 +1,18 @@
 const invertedIndexTest = new InvertedIndex();
 
-describe ( "Inverted Index Test", function(){
-    describe("Is createIndex properly called?", function(){
-        
+describe ( "Inverted Index Test", () =>{
+    describe("Is createIndex properly called?", () => {
+        it("should throw, Improper arguements", () => {
+            expect(() => {invertedIndexTest.createIndex()} )
+            .toThrow("improper arguement")
+            expect(() => {invertedIndexTest.createIndex(valid)})
+            .toThrow("improper arguement");
+        })
+        it("should throw, Empty JSON array", () => {
+            expect(() => {invertedIndexTest.
+                createIndex("string", [])})
+                .toThrow("Empty JSON array");
+
+        })
     })
 })
