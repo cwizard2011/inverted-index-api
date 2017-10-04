a271ce21a555ea1f05c7974c6ecc085eea68ec75
Cwizard <cwizard2011@gmail.com>
2017-10-04 09:20:49 +0100

    inverted-index

---
+++ src/inverted-index.js
@@ -0,0 +1,35 @@
+"use strict";
+/** Inverted Index Class
+ * @Author: Adeoye Peter Adeola <cwizard2011@gmail.com>
+ * @class
+ */
+class InvertedIndex {
+    /** @constructor
+     * initialises to empty object
+     */
+    constructor () {
+        this.indices = {};
+    }
+    /** check if the file is Malformed
+     * @param{object}, json file
+     * result return boolean
+     */
+    static isFileMalformed(jsonFile){
+        let result = false;
+        for(let i=0; i<jsonFile.length; i+=1){
+            result = !("title" in jsonFile[i] 
+            && "text" in jsonFile[i])
+            if (result) break;
+        }
+        return result;
+    }
+    /** Split string of text into unique word
+     * and form array from it
+     * @param{text},text to be splitted
+      */
+    static arrayFromText(text){
+        /**code goes here */
+    }
+
+
+}
--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -1,18 +1,56 @@
+"use strict";
+/**Inverted index test 
+ * @Author: Adeoye Peter Adeola
+ */
+import InvertedIndex from '../src/inverted-index.js'
+import valid from '../fixtures/valid.json'
+import book1 from '../fixtures/book1.json'
+import malformed from '../fixtures/malformed.json'
+import bad from '../fixtures/bad.json'
+import searchValid from '../fixtures/search-valid.json'
+import searchAll from '../fixtures/search-all.json'
+
 const invertedIndexTest = new InvertedIndex();
 
-describe ( "Inverted Index Test", () =>{
-    describe("Is createIndex properly called?", () => {
-        it("should throw, Improper arguements", () => {
-            expect(() => {invertedIndexTest.createIndex()} )
-            .toThrow("improper arguement")
-            expect(() => {invertedIndexTest.createIndex(valid)})
-            .toThrow("improper arguement");
+describe ('Inverted Index Test', () =>{
+    describe('Is createIndex properly called?', () => {
+        it('should throw, Improper arguements', () => {
+            expect(() => {invertedIndexTest.createIndex();} )
+            .toThrow('improper arguement');
+            expect(() => {invertedIndexTest.createIndex(valid); })
+            .toThrow('improper arguement');
         })
-        it("should throw, Empty JSON array", () => {
+        it('should throw, Empty JSON array', () => {
             expect(() => {invertedIndexTest.
-                createIndex("string", [])})
-                .toThrow("Empty JSON array");
+                createIndex('string', []);})
+                .toThrow('Empty JSON array');
 
         })
+        it('should throw, not JSON array', () =>{
+            expect(() => {invertedIndexTest.createIndex('string',3); })
+            .toThrow('not JSON array');
+        })
+        it('should throw, not JSON array', () =>{
+            expect(() => {invertedIndexTest.createIndex('string',{}); })
+            .toThrow('not JSON array');
+        })
+        it('should throw, Improper file name', () => {
+            expect(() => {invertedIndexTest.createIndex([], []); })
+            .toThrow('Improper file name');
+        })
+        it('should throw, Malformed file', () => {
+            expect(() => {invertedIndexTest
+                .createIndex('string', ['title']); })
+                .toThrow('Malformed file');
+            
+            expect(() => {invertedIndexTest.
+                createIndex('string', malformed)})
+                .toThrow('Malformed file');
+            expect(() => {invertedIndexTest.createIndex('string',bad); })
+            .toThrow(Error('Malformed file'));
+            
+
+        })
+
     })
 })
