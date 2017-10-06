9ae2195c4b9a0ebde282b9cf5c5c5797492b17ba
Cwizard <cwizard2011@gmail.com>
2017-10-06 22:44:20 +0100

    fix

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -38,7 +38,7 @@ class InvertedIndex {
   static bookContent(jsonFile) {
       let bookedContent = '';
       jsonFile.forEach((book) => {
-        bookedContent += `${book.title} ${book.text}`;
+      bookedContent += `${book.title} ${book.text}`;
     })
     return bookedContent.trim();
     }
@@ -48,13 +48,13 @@ class InvertedIndex {
    *  @param {string} fileContent, the content of the JSON array
    *  @returns {object} returns a string
    */
-    createIndex(fileName, fileContent) {
-        if (!fileName || fileContent === undefined) {
+  createIndex(fileName, fileContent) {
+    if (!fileName || fileContent === undefined) {
       throw new Error('Improper arguements');
-        }
-        if (!Array.isArray(fileContent)) {
-            throw new Error('Not JSON array');
-        }
+    }
+    if (!Array.isArray(fileContent)) {
+      throw new Error('Not JSON array');
+    }
     if (!fileContent.length) {
       throw new Error('Empty JSON array');
         }
@@ -64,71 +64,69 @@ class InvertedIndex {
         }
     } catch (err) {
         throw new Error('Malformed file');
-      }
-        
-        const index = {};
+      }    
+    const index = {};
     const allFileContent = InvertedIndex.arrayFromText(InvertedIndex.bookContent(fileContent));
-        let eachContent;
+    let eachContent;
     
     fileContent.forEach((book, filePath) => {
-        eachContent = book;
-      eachContent = 
-        new Set(`${eachContent.title} ${eachContent.text} `);
+      eachContent = book;
+      eachContent = new Set(`${eachContent.title} ${eachContent.text} `);
 
-            allFileContent.forEach((word) => {
-                if (eachContent.has(word)) {
-                    if (word in index) index[word].push(filePath);
-          else index[word] = [filePath];
-                }
-            })
-        })
-        this.indices[fileName] = index;
-        return JSON.stringify(index);
+    allFileContent.forEach((word) => {
+      if (eachContent.has(word)) {
+        if (word in index) index[word].push(filePath);
+        else index[word] = [filePath];
+        }
+      })
+    })
+    this.indices[fileName] = index;
+    return JSON.stringify(index);
     }
-    /** Method to search the already created index
-     *  @param {object} indices - indices to be searched
-     *  @param {string} fileName - name of the file to be
-     *  searched
-     *  @param {string|array} terms - search terms
-     *  @returns {object} - contains the location of each
-     *  terms
-     */
+  /** Method to search the already created index
+   *  @param {object} indices - indices to be searched
+   *  @param {string} fileName - name of the file to be
+   *  searched
+   *  @param {string|array} terms - search terms
+   *  @returns {object} - contains the location of each
+   *  terms
+   */
   static searchIndex(indices, ...terms) {
-      let searchTerms = [];
-      const result = {};
-      const keys = Object.keys(indices);
-      terms.forEach((term) => {
-          if (Array.isArray(term)) {
-              searchTerms.push(...term);
-            }
-          if (typeof term === 'string') {
-              searchTerms.push(...InvertedIndex.arrayFromText(term));
-            };
+    let searchTerms = [];
+    const result = {};
+    const keys = Object.keys(indices);
+    terms.forEach((term) => {
+      if (Array.isArray(term)) {
+        searchTerms.push(...term);
+      }
+      if (typeof term === 'string') {
+        searchTerms.push(...InvertedIndex.arrayFromText(term));
+      };
         });
-        keys.forEach((index) => {
-            result[index] = {};
-        });
-        let fileName;
-        if (searchTerms[1] === 'json') {
-            fileName = `${searchTerms[0]}.json`;
-            searchTerms = searchTerms.slice(2);
-        } else {
-            fileName = 'all';
+    keys.forEach((index) => {
+      result[index] = {};
+    });
+    let fileName;
+      if (searchTerms[1] === 'json') {
+        fileName = `${searchTerms[0]}.json`;
+        searchTerms = searchTerms.slice(2);
+      } else {
+          fileName = 'all';
         }
-        keys.forEach((index) => {
-            searchTerms.forEach((word) => {
-                if (word in indices[index]) {
-                    result[index][word] = indices[index][word];
-                }
-            });
-        });
-        if (!(fileName in indices) && fileName !== 'all') {
-            return `${fileName} not in index`;
+    keys.forEach((index) => {
+      searchTerms.forEach((word) => {
+        if (word in indices[index]) {
+          result[index][word] = indices[index][word];
         }
-        if (fileName === 'all') {
-            return result;
-        }
-        return result[fileName];
+      });
+    });
+    if (!(fileName in indices) && fileName !== 'all') {
+      return `${fileName} not in index`;
+    }
+    if (fileName === 'all') {
+      return result;
+    }
+    return result[fileName];
     }
 }
 
