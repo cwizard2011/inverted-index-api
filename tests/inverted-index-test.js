263273b63658546640b9699a07374f81629d7cac
Cwizard <cwizard2011@gmail.com>
2017-10-06 20:13:19 +0100

    fix

--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -15,17 +15,17 @@ describe('Inverted Index Test', () => {
     describe('Is createIndex properly called?', () => {
         it('should throw, `Improper arguements`', () => {
             expect(invertedIndexTest.createIndex()).toThrow('improper arguement');
-            expect(invertedIndexTest.createIndex(valid)).toThrow('improper arguement');
-        })
-        it('should throw, `Empty JSON array`', () => {
-            expect(invertedIndexTest.createIndex('string', [])).toThrow('Empty JSON array');
+      expect(invertedIndexTest.createIndex(valid)).toThrow('improper arguement');
+  })
+    it('should throw, `Empty JSON array`', () => {
+        expect(() => { invertedIndexTest.createIndex('string', []); }).toThrow('Empty JSON array');
 
         })
         it('should throw, `not JSON array`', () => {
       expect(invertedIndexTest.createIndex('string', 3)).toThrow('not JSON array');
         })
         it('should throw,` not JSON array`', () => {
-            expect(invertedIndexTest.createIndex('string' ,{ })).toThrow('not JSON array');
+            expect(() => { invertedIndexTest.createIndex('string' ,{ }); }).toThrow('not JSON array');
         })
         it('should throw, `Improper file name`', () => {
       expect(invertedIndexTest.createIndex([], [])).toThrow('Improper file name');
@@ -36,7 +36,6 @@ describe('Inverted Index Test', () => {
             expect(invertedIndexTest.createIndex('string', malformed)).toThrow('Malformed file');
             expect(invertedIndexTest.createIndex('string', bad)).toThrow(Error('Malformed file'));
         });
-
     });
     describe('Checks If file is malformed', () => {
         it('should return `true` for malformed JSON file', () => {
