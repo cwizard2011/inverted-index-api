f011d08606d45b68eae415b3d3a42387658e5b39
Cwizard <cwizard2011@gmail.com>
2017-10-08 13:45:42 +0100

    fix

--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -8,20 +8,20 @@ import malformed from '../fixtures/malformed.json';
 import bad from '../fixtures/bad.json';
 import searchValid from '../fixtures/search-valid.json';
 import searchAll from '../fixtures/search-all.json';
-/* eslint no-undef: "error" */
+
 const invertedIndexTest = new InvertedIndex();
 
 describe('Inverted Index Test', () => {
   describe('Is createIndex properly called?', () => {
     it('should throw, `Improper arguements`', () => {
-      expect(() => { invertedIndexTest.createIndex(); }).toThrow('improper arguement');
-      expect(() => { invertedIndexTest.createIndex(valid); }).toThrow('improper arguement');
+      expect((invalid) => { invertedIndexTest.createIndex(); }).toThrow('improper arguement');
+      expect((invalid) => { invertedIndexTest.createIndex(valid); }).toThrow('improper arguement');
     });
     it('should throw, `Empty JSON array`', () => {
-      expect(() => { invertedIndexTest.createIndex('string', []); }).toThrow('Empty JSON array');
+      expect((empty) => { invertedIndexTest.createIndex('string', []); }).toThrow('Empty JSON array');
   });
     it('should throw, `not JSON array`', () => {
-      expect(invertedIndexTest.createIndex('string', 3)).toThrow('not JSON array');
+      expect((nonArray) => {invertedIndexTest.createIndex('string', 3); }).toThrow('not JSON array');
   });
     it('should throw,` not JSON array`', () => {
       expect(() => { invertedIndexTest.createIndex('string', {}); }).toThrow('not JSON array'); 
